b9dbdb4fdbbff97858e3aafe906f03c56a21b633
qiaojushuang <747848783@qq.com>
2019-02-16 10:53:40 +0800

    3

---
+++ offer/3.go
@@ -0,0 +1,19 @@
+package offer
+
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	rows, cols := len(matrix), len(matrix[0])
+	row, col := rows-1, 0
+	for row >= 0 && col < cols {
+		if matrix[row][col] == target {
+			return true
+		} else if matrix[row][col] > target {
+			row -= 1
+		} else {
+			col += 1
+		}
+	}
+	return false
+}
