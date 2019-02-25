07d90dca3e8efbf8d42bf6b007acffd35574bca5
qiaojushuang <747848783@qq.com>
2019-02-25 23:29:22 +0800

    2

---
+++ leetcode/2.go
@@ -0,0 +1,29 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{}
+	tail := head
+	carry := 0
+	for l1 != nil || l2 != nil || carry != 0 {
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+		tail.Next = &ListNode{Val: carry % 10}
+		carry /= 10
+		tail = tail.Next
+	}
+	return head.Next
+}
